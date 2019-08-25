7e122b3fc28b73c06cf9d71a1b6edb74af3f52ec
Erbil <erbil.silik@kolayik.com>
2019-08-25 08:42:17 +0300

    Specify node version

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('/api');
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 
   if (module.hot) {
     module.hot.accept();
