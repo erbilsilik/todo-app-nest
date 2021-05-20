019254311f6a13c491b72f7e3cf11c182841f173
erbilsilik <erbil.silik@yandex.com>
2021-05-20 15:44:11 +0300

    Update database.providers.ts

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -4,6 +4,6 @@ export const databaseProviders = [
     {
         provide: 'DATABASE_CONNECTION',
         useFactory: async (): Promise<typeof mongoose> =>
-            await mongoose.connect('mongodb+srv://todo:123reklamlar@cluster0-bdmth.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useFindAndModify: false }),
+            await mongoose.connect('', { useNewUrlParser: true, useFindAndModify: false }),
     },
 ];
